290d95ce158052c515e7d7cd62c0995a8dbe465a
Luana <ldeoliveiratech@gmail.com>
2023-04-25 17:48:48 +0200

    Section fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
 
 import './App.css'
 import movies from './assets/movies.json'
-import Section from './components/Section'
 
 const App = () => {
   return movies.map((elem) => {
     return (
-      <Section key = {elem.category} image = {image}/>
+      <section key={elem.category}>
+        <h2>{elem.category}</h2>
+        {elem.images.map((image) => {
+          return (
+            <img key={image} src={image} alt="" />
+          )
+        }) }
+      </section>
     )
   }) 
 }
 
-{/* <section key={elem.category}>
-  <h2>{elem.category}</h2>
-  {elem.images.map((image) => {
-    return (
-      <img key={image} src={image} alt="" />
-    )
-  }) }
-</section> */}
+// const App = () => {
+//   return movies.map((elem) => {
+//     return (
+//       <section key={elem.category}>
+//         <h2>{elem.category}</h2>
+//         {elem.images.map((image) => {
+//           return (
+//             <img key={image} src={image} alt="" />
+//           )
+//         }) }
+//       </section>
+//     )
+//   }) 
+// }
 
 export default App
